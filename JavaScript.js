7f495b5e75e0c3bfd8aef9c536d87269df93de3a
Lesley-Middelkamp <73035796+Lesley-Middelkamp@users.noreply.github.com>
2021-01-10 12:07:49 -0500

    update weather

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,4 @@
+               
 function formatDate(date) {
   let hours = date.getHours();
   if (hours < 10) {
@@ -23,95 +24,75 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function showWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+ let h1 = document.querySelector("h1");
+  let temperature = Math.round(response.data.main.temp);
+  place.innerHTML = `It is currently ${temperature}° in ${response.data.name}`;
+  console.log(temperature)
+  console.log(response)
+}
+
+function dispCel(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celTemp);
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
 function searchCity(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeather);
-   apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(dispalyForecast);
 }
-
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
   searchCity(city);
 }
 
+function currentLocation(position){
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showWeather);
+}
+
+function getCurrentLocation(){
+   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+   let lat = position.coords.latitude;
+   let lon = position.coords.longitude;
+   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+   }
+    navigator.geolocation.getCurrentPosition(currentLocation);
+    
+
+function dispFahr(event) {
+  event.preventDefault();
+  var windElement = document.querySelector("#wind");
+  let fahrTemp = (celTemp * 9) / 5 + 32;
+  windElement.innerHTML = Math.round(fahrTemp);
+}
+
+function dispCel(event) {
+  event.preventDefault();
+  var windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(celTemp);
+}
+
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
-
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
 
-
-function showWeather(response) {
-  let h1 = document.querySelector("h1");
-  let temperature = Math.round(response.data.main.temp);
-  h1.innerHTML = `It is currently ${temperature}°C in ${response.data.name}`;
-  console.log(temperature)
-  console.log(response)
-}
-
-function retrievePosition(position) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-  axios.get(url).then(showWeather);
-
-}
-
-navigator.geolocation.getCurrentPosition(retrievePosition); 
-
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon");
-
-  celTemp = response.data.main.temp;
-  
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
-}
-
-
-
-function dispFahr(event){
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  celLink.classList.remove("active");
-  fahrLink.classList.add("active");
-  let fahrTemp = (celTemp * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrTemp);
-}      
-function dispCel(event){
-  event.preventDefault();
-  celLink.classList.add("active");
-  fahrLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celTemp);
-}
-let fahrLink = document.querySelector("#fahrenheit-link");
-fahrLink.addEventListener("click", dispFahr);
-
-let celLink = document.querySelector("#celsius-link");
-celLink.addEventListener("click", dispCel);
-
-let celTemp = null;
 
